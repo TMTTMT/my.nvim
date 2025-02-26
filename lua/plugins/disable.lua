55cc756c6609b4ec95c0b5e6a7b9942bcbd03dec
Thong Thai <44636622+TMTTMT@users.noreply.github.com>
2025-02-26 15:44:06 +0700

    remove default plugins

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,8 @@
+return {
+    -- disable default LazyVim plugins
+    {"folke/ts-comments.nvim", enabled = false},
+    {"catppuccin/nvim", enabled = false},
+    {"tpope/vim-dadbod", enabled = false},
+    {"tpope/vim-dadbod-ui", enabled = false},
+    {"windwp/nvim-ts-autotag", enabled = false},
+}
